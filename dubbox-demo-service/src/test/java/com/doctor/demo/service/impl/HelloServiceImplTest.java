967ea5bcd88e7400d9eeb78f97406605a4ae44c7
sdcuike <sdcuike@aol.com>
2016-01-26 18:07:31 +0800

    Dubbo RPC中序列化使用组合：Dubbo: kyro

--- dubbox-demo-service/src/test/java/com/doctor/demo/service/impl/HelloServiceImplTest.java
+++ dubbox-demo-service/src/test/java/com/doctor/demo/service/impl/HelloServiceImplTest.java
@@ -1,11 +1,11 @@
 package com.doctor.demo.service.impl;
 
-import com.alibaba.dubbo.container.spring.SpringContainer;
-
 public class HelloServiceImplTest {
 
     public static void main(String[] args) {
-        SpringContainer springContainer = new com.alibaba.dubbo.container.spring.SpringContainer();
-        springContainer.start();
+        // SpringContainer springContainer = new
+        // com.alibaba.dubbo.container.spring.SpringContainer();
+        // springContainer.start();
+        com.alibaba.dubbo.container.Main.main(args);
     }
 }
