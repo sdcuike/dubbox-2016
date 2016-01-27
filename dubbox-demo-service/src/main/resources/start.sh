19351061f80dbf869d6c7dc992c99d1551df737c
sdcuike <sdcuike@aol.com>
2016-01-27 16:40:40 +0800

    update start.sh

--- dubbox-demo-service/src/main/resources/start.sh
+++ dubbox-demo-service/src/main/resources/start.sh
@@ -3,6 +3,6 @@ echo dubbo services are starting...
 echo JAVA_HOME:$JAVA_HOME
 echo PATH:$PATH
 
-nohup java -classpath  .:$CLASSPATH:../lib/* com.doctor.demo.service.boot.BootMain >& /dev/null   &
+nohup java -classpath  .:$CLASSPATH:../lib/* com.alibaba.dubbo.container.Main  >& /dev/null   &
 
 echo $! > service.pid
