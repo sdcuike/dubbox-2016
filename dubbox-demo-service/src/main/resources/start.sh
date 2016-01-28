8a6a0d96c12236be0231940870f1170d590d2931
sdcuike <sdcuike@aol.com>
2016-01-28 11:11:54 +0800

    jvm参数更改

--- dubbox-demo-service/src/main/resources/start.sh
+++ dubbox-demo-service/src/main/resources/start.sh
@@ -21,7 +21,30 @@ if [ -n "$PIDS" ]; then
 fi
 ###########end
 
+####jvm参数设置
+JAVA_OPTS=" -Djava.awt.headless=true -Djava.net.preferIPv4Stack=true "
 
-nohup java    -classpath  $deployDir/config/*:$deployDir/lib/* com.alibaba.dubbo.container.Main  >& /dev/null   &
+JAVA_DEBUG_OPTS=""
+if [ "$1" = "debug" ]; then
+    JAVA_DEBUG_OPTS=" -Xdebug -Xnoagent -Djava.compiler=NONE -Xrunjdwp:transport=dt_socket,address=8000,server=y,suspend=n "
+fi
+
+JAVA_JMX_OPTS=""
+if [ "$1" = "jmx" ]; then
+    JAVA_JMX_OPTS=" -Dcom.sun.management.jmxremote.port=1099 -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false "
+fi
+
+JAVA_MEM_OPTS=""
+BITS=`java -version 2>&1 | grep -i 64-bit`
+if [ -n "$BITS" ]; then
+    JAVA_MEM_OPTS=" -server -Xmx2g -Xms2g -Xmn256m   -Xss256k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled   -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 "
+else
+    JAVA_MEM_OPTS=" -server -Xms1g -Xmx1g   -XX:SurvivorRatio=2 -XX:+UseParallelGC "
+fi
+
+######
+
+nohup java  $JAVA_OPTS $JAVA_MEM_OPTS $JAVA_DEBUG_OPTS $JAVA_JMX_OPTS  -classpath  $deployDir/config/*:$deployDir/lib/* com.alibaba.dubbo.container.Main  >& /dev/null   &
+
 
 echo $! > bin/service.pid
