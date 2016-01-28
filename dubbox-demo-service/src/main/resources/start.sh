a45c4833ac86191a823f7b49abac096485d6a402
sdcuike <sdcuike@aol.com>
2016-01-28 10:55:35 +0800

    启动脚本修改（避免重复启动，日志路径修改）

--- dubbox-demo-service/src/main/resources/start.sh
+++ dubbox-demo-service/src/main/resources/start.sh
@@ -1,8 +1,27 @@
 #!/bin/bash
+
+currentDir=$(pwd)
+echo currentDir $currentDir
+
+cd ..
+deployDir=$(pwd)
+echo deployDir $deployDir
+
 echo dubbo services are starting...
 echo JAVA_HOME:$JAVA_HOME
 echo PATH:$PATH
 
-nohup java -classpath  .:$CLASSPATH:../lib/* com.alibaba.dubbo.container.Main  >& /dev/null   &
 
-echo $! > service.pid
+########### beign服务启动了吗(避免重复启动)
+PIDS=$( ps -f | grep java | grep "$deployDir" |awk '{print $2}' )
+if [ -n "$PIDS" ]; then
+    echo "ERROR: The $deployDir already have server started!"
+    echo "PID: $PIDS"
+    exit 1
+fi
+###########end
+
+
+nohup java    -classpath  $deployDir/config/*:$deployDir/lib/* com.alibaba.dubbo.container.Main  >& /dev/null   &
+
+echo $! > bin/service.pid
