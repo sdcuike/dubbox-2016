89e41f6836166123b5c28e0d9e9576b0b22b9e6f
sdcuike <sdcuike@aol.com>
2016-02-01 11:26:12 +0800

    update stop.sh kill －9

--- dubbox-demo-service/src/main/resources/stop.sh
+++ dubbox-demo-service/src/main/resources/stop.sh
@@ -1,1 +1,23 @@
-kill `cat service.pid`
+currentDir=$(pwd)
+echo currentDir $currentDir
+
+cd ..
+deployDir=$(pwd)
+echo deployDir $deployDir
+
+echo dubbo services are stoping...
+echo JAVA_HOME:$JAVA_HOME
+
+kill $(cat bin/service.pid )  
+
+
+########### beign服务关闭了吗(避免kill 失败情况下 $?得不到正确的返回值)
+
+PIDS=$( ps -f | grep java | grep "$deployDir" |awk '{print $2}' )
+if [ -n "$PIDS" ]; then
+    echo "ERROR: The $deployDir server stop failure! and kill -9 stoping server"
+    echo "PID: $PIDS"
+    kill -9 $PIDS
+fi
+
+###########end
