1cdbb0b0f1707142be434f48c97d427f6cd6904c
sdcuike <sdcuike@aol.com>
2016-02-19 14:31:50 +0800

    加入dubbo-monitor

--- dubbox-demo-client/src/test/java/com/doctor/demo/service/client/impl/HelloServiceClientImpl2Test.java
+++ dubbox-demo-client/src/test/java/com/doctor/demo/service/client/impl/HelloServiceClientImpl2Test.java
@@ -11,21 +11,31 @@ public class HelloServiceClientImpl2Test {
 
     public static void main(String[] args) throws InterruptedException {
         new Thread(() -> com.alibaba.dubbo.container.Main.main(args)).start();
+        new Thread(HelloServiceClientImpl2Test::run).start();
 
-        TimeUnit.SECONDS.sleep(10);
-        HelloService helloService = (HelloService) SpringContainer.getContext().getBean("helloServiceClient");
+    }
 
-        WelcomeDto welcomDto = new WelcomeDto();
-        welcomDto.setAge(10088);
-        welcomDto.setName("doctor who");
-        WelcomeResponseDto welcomeResponseDto = helloService.hello(welcomDto);
-        System.err.println(welcomeResponseDto);
+    public static void run() {
+        while (true) {
+            try {
+                TimeUnit.SECONDS.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            HelloService helloService = (HelloService) SpringContainer.getContext().getBean("helloServiceClient");
 
-        welcomeResponseDto = helloService.hello("你好");
-        System.err.println(welcomeResponseDto);
+            WelcomeDto welcomDto = new WelcomeDto();
+            welcomDto.setAge(10088);
+            welcomDto.setName("doctor who");
+            WelcomeResponseDto welcomeResponseDto = helloService.hello(welcomDto);
+            System.err.println(welcomeResponseDto);
 
-        welcomeResponseDto = helloService.hello("rain", 888, welcomDto);
-        System.err.println(welcomeResponseDto);
+            welcomeResponseDto = helloService.hello("你好");
+            System.err.println(welcomeResponseDto);
+
+            welcomeResponseDto = helloService.hello("rain", 888, welcomDto);
+            System.err.println(welcomeResponseDto);
+        }
 
     }
 
