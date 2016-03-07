9d7d2989b28d5e5836b7dbd6e3ef216c34c56bec
sdcuike <303286730@qq.com>
2016-03-07 14:51:35 +0800

    enunciate-maven-plugin  not support Swagger-Core Annotations

--- dubbox-demo-common/src/main/java/com/doctor/demo/service/HelloRestService.java
+++ dubbox-demo-common/src/main/java/com/doctor/demo/service/HelloRestService.java
@@ -1,40 +1,31 @@
 package com.doctor.demo.service;
 
-import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import com.doctor.demo.common.dto.WelcomeDto;
 import com.doctor.demo.common.dto.WelcomeResponseDto;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiParam;
-import io.swagger.annotations.ApiResponse;
 
 /**
  * @author sdcuike
  *
  * @time 2016年1月25日 下午11:07:44
  */
-@Api(value = "hello", tags = "hello service")
 @Path("hello")
 public interface HelloRestService {
     /**
      * 老接口方式（DTO)
+     * xxx不能为空
      * 
      * @param welcomDto
      * @return
      */
-    @ApiOperation(value = "test hello method", notes = "note")
 
     @POST
     @Path("w")
-    @Consumes({ MediaType.APPLICATION_JSON })
-    @Produces({ MediaType.APPLICATION_JSON })
-    @ApiResponse(message = "ok", code = 200)
-    WelcomeResponseDto hello(@ApiParam(value = "welcomDto object", required = true) WelcomeDto welcomDto);
+    @ApiOperation(value = "test hello ", notes = "test swagger")
+    WelcomeResponseDto hello(WelcomeDto welcomDto);
 
 }
