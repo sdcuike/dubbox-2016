631e1709ee93547cecccaf15f63517f9d4bce784
sdcuike <sdcuike@aol.com>
2016-03-09 18:53:47 +0800

    update BraveFilter code

--- dubbox-demo-web/src/main/java/com/doctor/demo/filter/BraveFilter.java
+++ dubbox-demo-web/src/main/java/com/doctor/demo/filter/BraveFilter.java
@@ -12,6 +12,15 @@ import javax.servlet.annotation.WebFilter;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContext;
+
+import com.alibaba.dubbo.rpc.RpcContext;
+import com.doctor.dubbo.extend.brave.DubboProviderRequestAdapter;
+import com.doctor.dubbo.extend.brave.DubboProviderResponesAdapter;
+import com.doctor.dubbo.extend.brave.DubboSpanNameProvider;
+import com.doctor.dubbo.extend.brave.helper.ApplicationContextProvider;
+import com.github.kristofa.brave.Brave;
+import com.github.kristofa.brave.http.BraveHttpHeaders;
 
 /**
  * @author sdcuike
@@ -21,6 +30,7 @@ import org.slf4j.LoggerFactory;
 @WebFilter(urlPatterns = "/*")
 public class BraveFilter implements Filter {
     private Logger log = LoggerFactory.getLogger(BraveFilter.class);
+    private static final String spanName = "demo-web";
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
@@ -29,8 +39,24 @@ public class BraveFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info(getClass() + " doFilter------------");
-        chain.doFilter(request, response);
+        ApplicationContext context = ApplicationContextProvider.getApplicationContext();
+
+        Brave brave = context.getBean(Brave.class);
+        DubboSpanNameProvider spanNameProvider = context.getBean(DubboSpanNameProvider.class);
+        RpcContext rpcContext = RpcContext.getContext();
+
+        rpcContext.setAttachment(BraveHttpHeaders.Sampled.getName(), "true");
+        // rpcContext.setAttachment(BraveHttpHeaders.TraceId.getName(), IdConversion.convertToString(1L));
+        // rpcContext.setAttachment(BraveHttpHeaders.SpanId.getName(), IdConversion.convertToString(1L));
+
+        brave.serverRequestInterceptor().handle(new DubboProviderRequestAdapter(rpcContext, spanNameProvider, spanName));
+
+        try {
+            chain.doFilter(request, response);
+        } finally {
+            brave.serverResponseInterceptor().handle(new DubboProviderResponesAdapter(null));
+        }
+
     }
 
     @Override
